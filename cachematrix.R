db6c7728aebad7abe8813d2f4aa1bb7a85a67577
VBeutgen <vbeutgen@gmail.com>
2022-08-03 14:59:53 +0200

    second commit of cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2
 
 
 
-## Write a short comment describing this function
-# makeCacheMatrix creates a set of functions and returns them to the parent environment as a list
+
+
+## makeCacheMatrix creates a set of functions and returns them to the parent environment as a list
 
 
 
@@ -17,19 +17,16 @@ makeCacheMatrix <- function(x = matrix()) { # x initialized as function object, 
       }
       
       get <- function() x # accesses x from parent environment
-      
       setInv <- function(solve) invMat <<- solve # assigns input (solve) to invMat
-      
       getInv <- function() invMat # access invMat and retrieve its value
-      
       list(set = set, get = get,# creates named list, returned to parent environment
            setInv = setInv,
            getInv = getInv)
 }
 
 
-## Write a short comment describing this function:
-# Return a matrix that is the inverse of 'x'. First tries to access value from cache. If no value is stored, it is calculated and then cached
+
+## Return a matrix that is the inverse of 'x'. First tries to access value from cache. If no value is stored, it is calculated and then cached
 
 cacheSolve <- function(x, ...){ # requires input in form of makeCacheMatrix
       invMat <- x$getInv() # try to retrieve inverse matrix from object passed as argument
